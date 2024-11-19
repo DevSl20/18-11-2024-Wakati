7ecff7e18f48e13517c212dff1320263bfd99c40
Devang Salvi <devangsalvi21@gmail.com>
2024-11-19 07:36:18 +0000

    post and get reqs

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  console.log(c.req.query("Hello Hono!"));
+app.use(logger()); // Enable logger middleware
+const wpm = 238;
 
-const text = c.req.query('text')
+app.get("/status", (c) => {
+  return c.json({ status: "API is Active" });
+});
 
+app.get("/", (c) => {
+   const text = c.req.query('text')
+if (!text) {
+  return c.json({ message: "Please provide a text" }, 400);
+}
+
+return c.json(text);
+});
+
+app.post("/", async (c) => {
+  const { text } = await c.req.json();
   if (!text) {
-    c.status(400);
-    return c.json ({error: "Field text is required"});
+    return c.json({ message: "Please provide a text" }, 400);
   }
-  const length = text.split(/\.s/).length;
-return c.json({length})
+  return c.json({count: text.split(/\.s/).length});
+});
+// app.get('/', (c) => {
+//   console.log(c.req.query("Hello Hono!"));
 
-})
+// const text = c.req.query('text')
 
-export default app
+//   if (!text) {
+//     c.status(400);
+//     return c.json ({error: "Field text is required"});
+//   }
+//   const length = text.split(/\.s/).length;
+// return c.json({length})
+
+// })
+
+export default app;
